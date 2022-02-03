d1e0cfe38a42b2c9a61b7a2ffd65fed514845696
Carlos Eduardo <carlos.eduardo@estudante.ifgoiano.edu.br>
2022-02-03 10:20:40 -0300

    ajustes nos event listeners

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,31 @@
-const botaoEnviar = document.querySelector("[data-botaoEnviar]");
+const botaoCriarUsuario = document.querySelector("[data-botaoCriar]");
 const botaoDeletar = document.querySelector("[data-botaoDeletar");
 const botaoEditar = document.querySelector("[data-botaoEditar");
 
-console.log(botaoEnviar);
-console.log(botaoDeletar);
-console.log(botaoEditar);
+const formularioCadastro = document.querySelector("[data-formularioCadastro]");
+const formularioEdit = document.querySelector("[data-formularioEdita]");
+
+const botaoPesquisa = document.querySelector(".barra-pesquisa__back");
+const barraPesquisa = document.querySelector("[data-barraPesquisa]");
 
 function botaoCriarUsuarioHandler(e) {
   console.log(e.target);
 }
-botaoEnviar.addEventListener("click", botaoCriarUsuarioHandler);
-botaoDeletar.addEventListener("click", botaoCriarUsuarioHandler);
-botaoEditar.addEventListener("click", botaoCriarUsuarioHandler);
+
+function botaoDeletarUsuarioHandler(e) {
+  console.log(e.target);
+}
+
+function botaoEditarUsuarioHandler(e) {
+  console.log(e.target);
+}
+
+function barraPesquisaHandler(e) {
+  console.log(barraPesquisa.value);
+}
+
+botaoPesquisa.addEventListener("click", barraPesquisaHandler);
+
+botaoCriarUsuario.addEventListener("click", botaoCriarUsuarioHandler);
+botaoDeletar.addEventListener("click", botaoDeletarUsuarioHandler);
+botaoEditar.addEventListener("click", botaoEditarUsuarioHandler);
