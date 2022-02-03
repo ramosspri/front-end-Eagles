e44e774ca2143b9d9b288d0e4a3027714a9112af
Carlos Eduardo <carlos.eduardo@estudante.ifgoiano.edu.br>
2022-02-03 11:33:14 -0300

    implementação inicial do formData

--- javascript/script.js
+++ javascript/script.js
@@ -2,14 +2,25 @@ const botaoCriarUsuario = document.querySelector("[data-botaoCriar]");
 const botaoDeletar = document.querySelector("[data-botaoDeletar");
 const botaoEditar = document.querySelector("[data-botaoEditar");
 
-const formularioCadastro = document.querySelector("[data-formularioCadastro]");
+const formularioCadastro = document.querySelectorAll(
+  "[data-formularioCadastro] .input"
+);
 const formularioEdit = document.querySelector("[data-formularioEdita]");
 
 const botaoPesquisa = document.querySelector(".barra-pesquisa__back");
 const barraPesquisa = document.querySelector("[data-barraPesquisa]");
 
 function botaoCriarUsuarioHandler(e) {
-  console.log(e.target);
+  e.preventDefault();
+  const formularioCadastroObj = new FormData();
+
+  formularioCadastroObj.append("nome", formularioCadastro[0].value);
+  formularioCadastroObj.append("sobrenome", formularioCadastro[1].value);
+  formularioCadastroObj.append("dataNascimento", formularioCadastro[2].value);
+  console.log(formularioCadastroObj);
+  console.log(formularioCadastroObj.get("nome"));
+  console.log(formularioCadastroObj.get("sobrenome"));
+  console.log(formularioCadastroObj.get("dataNascimento"));
 }
 
 function botaoDeletarUsuarioHandler(e) {
